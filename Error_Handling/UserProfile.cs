ba0d274bda3a7ed6dc38e009c5a12b13ba7448e2
Dace Akmene <d.akmene@gmail.com>
2019-10-01 18:34:23 +0300

    User profile class created

---
+++ Error_Handling/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error_Handling
+{
+    class UserProfile
+    {
+        public string FullName;
+        public char Gender;
+        public DateTime Birthday;
+
+        public UserProfile(string fullName, char gender, DateTime birthday)
+        {
+            FullName = fullName;
+            Gender = gender;
+            Birthday = birthday;
+        }
+
+        public int Age(DateTime birthday)
+        {
+            int yearNow = DateTime.Today.Year;
+            int birthYear = birthday.Year;
+            int age = yearNow - birthYear;
+            return age;
+        }
+    }
+}
